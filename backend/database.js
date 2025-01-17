02cb9fced072edc1b5606e5bad0e047241b81865
Ali terekmani <60880747+aliterekamni12@users.noreply.github.com>
2025-01-17 14:31:32 +0200

    12

--- backend/database.js
+++ backend/database.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 module.exports.connectDB = async()=>{
     try {
-        await mongoose.connect(process.env.DB_URL);
+        await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log("Database connected")
     } catch (error) {
         console.log(error);
