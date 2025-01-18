2e893dab710a16cc2ba42cc6b9c460ead00f5f1b
Ali terekmani <60880747+aliterekamni12@users.noreply.github.com>
2025-01-18 15:22:07 +0200

    12

--- backend/index.js
+++ backend/index.js
@@ -14,17 +14,16 @@ const app = express();
 var cors = require('cors')
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 
 
 
 
 
-app.get('*', (req, res,next) => {
-    res.sendFile(path.join(__dirname, 'build/index.html'));
-    res.setHeader('Last-Modified', (new Date()).toUTCString());
-  next(); 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/build','index.html'));
+    
 });
 
 app.get('/test', (req, res) => {
